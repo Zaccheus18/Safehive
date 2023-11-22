2fb867e632f2add302e758a67c1893f10f5edcb1
Frenchie Chua <frenchiechua123@gmail.com>
2023-11-23 07:23:18 +0800

    Update the backend link

--- src/widgets/cards/draganddrop.jsx
+++ src/widgets/cards/draganddrop.jsx
@@ -69,11 +69,11 @@ const DragDropFile = ({
       formData.append("file", file);
       formData.append("threshold", threshold);
 
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://safehive-backend.onrender.com/predict", {
         method: "POST",
         body: formData,
       });
-
+      
       if (response.ok) {
         const data = await response.json();
         const { estimatedCount, crowdStatus, crowdDensityFrequency, crowdDensity } = data;
