d71a628d7ae803a9218413f0b9b497d6c2a4e1c6
Frenchie Chua <frenchiechua123@gmail.com>
2023-12-02 16:45:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ async def predict_crowd_density(file: UploadFile = File(...), threshold: int = F
 
     return response
 
-@app.get('/predict')
+@app.get('/{filename}')
 async def uploaded_file(filename):
     return FileResponse(os.path.join(UPLOAD_DIRECTORY, filename))
 
